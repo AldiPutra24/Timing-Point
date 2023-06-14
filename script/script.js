bcc04cd50e0219404ae68666801ab1741c5782e2
Aldi Putra_21.11.4369 <96532520+AldiPutra24@users.noreply.github.com>
2023-06-14 17:39:40 +0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,30 +1,29 @@
-const menubar = document.querySelector(".hamburger");
-const menunav = document.querySelector(".navbar");
-const menumuncul = document.querySelector(".menu");
+const menuBar = document.querySelector(".hamburger");
+const menuNav = document.querySelector(".navbar");
+const menuNavigation = document.querySelector(".menu");
+
+menuBar.addEventListener("click", () => {
+  menuBar.classList.toggle("is-active");
+  menuNavigation.classList.toggle("menu-active");
+});
+
+window.addEventListener("scroll", () => {
+  menuNav.classList.toggle("nav-scroll-active", window.scrollY > 0);
+
+  windowsPosition = window.scrollY > 200;
+  menuBar.classList.toggle("btn-active", windowsPosition);
+  menuBar.classList.remove("is-active");
+  menuNavigation.classList.remove("menu-active");
+});
+
 const loader = document.querySelector(".loader");
 
-
-menubar.addEventListener("click",() => {
-    menubar.classList.toggle("is-active");
-    menumuncul.classList.toggle("menu-active");
-});
-
-window.addEventListener("scroll",() => {
-    menunav.classList.toggle("nav-scroll-active", window.scrollY > 0);
-
-    windowPosition = window.scrollY > 200;
-    menubar.classList.toggle("btn-active", windowPosition);
-    menubar.classList.remove("is-active");
-    menumuncul.classList.remove("menu-active");
-});
-
-
 function loaderActive() {
-    loader.classList.add("loader-active");
+  loader.classList.add("loader-active");
 }
-  
+
 function loaderActiveTime() {
-    setInterval(loaderActive, 3000);
+  setInterval(loaderActive, 3000);
 }
-  
+
 window.onload = loaderActiveTime();
