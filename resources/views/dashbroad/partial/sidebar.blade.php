e45de9331e0156ecb82076363a841679832f8322
jessicaavg <jessica.vegaa@students.amikom.ac.id>
2023-07-04 23:33:22 +0700

    update

--- resources/views/dashbroad/partial/sidebar.blade.php
+++ resources/views/dashbroad/partial/sidebar.blade.php
@@ -9,31 +9,26 @@
     <div class="main-1">
       <div class="list-item-1 {{ ($title === "dashbroad" ? 'active': '') }}">
         <a href="/dashbroad">
-          <!-- <i class="fa-solid fa-gear fa-lg"></i> -->
           <span class="description-1">Dashbroad</span>
         </a>
       </div>
       <div class="list-item-1 {{ ($title === "harian" ? 'active': '') }}">
         <a href="/harian">
-            <!-- <i class="fa-solid fa-gear fa-lg"></i> -->
           <span class="description-1">To-Do List</span>
         </a>
       </div>
       <div class="list-item-1 {{ ($title === "isitugas" ? 'active': '') }}">
         <a href="/isitugas">
-            <!-- <i class="fa-solid fa-gear fa-lg"></i> -->
           <span class="description-1">Recap Of Duty</span>
         </a>
       </div>
       <div class="list-item-1 {{ ($title === "notepad" ? 'active': '') }}">
         <a href="notepad">
-            <!-- <i class="fa-solid fa-gear fa-lg"></i> -->
           <span class="description-1">Sticky Note</span>
         </a>
       </div>
       <div class="list-item-1 {{ ($title === "setting" ? 'active': '') }}">
         <a href="setting">
-            <!-- <i class="fa-solid fa-gear fa-lg"></i> -->
           <span class="description-1">Setting</span>
         </a>
       </div>
